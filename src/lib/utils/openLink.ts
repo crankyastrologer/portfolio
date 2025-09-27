5246fab116e65d0721ae9d498bbff019def9a3c3
Ansh Verma <94965780+crankyastrologer@users.noreply.github.com>
2025-09-27 17:41:37 +0530

    Update openLink.ts
    add new resume

--- src/lib/utils/openLink.ts
+++ src/lib/utils/openLink.ts
@@ -3,5 +3,5 @@
 		if (no == 2) window.open('https://www.linkedin.com/in/ansh-vermaai/', '_blank');
 		if (no == 3) window.open('https://github.com/crankyastrologer', '_blank');
 		if (no == 4)
-			window.open('https://utfs.io/f/47311458-cecc-450e-9daa-bd0364fd9afb-u30njs.pdf', '_blank');
+			window.open('https://kmvtiv3h5z.ufs.sh/f/z6mi9BYbVtivyt8DSG7CiEPSVtAHOFZDaLz7JG3plojf5sBc', '_blank');
 	}
